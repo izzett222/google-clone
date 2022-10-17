bf980c0d7fd3456f73f306baf927e9beb42f90a6
izzett222 <iizzeddin62@gmail.com>
2022-10-17 14:59:10 +0200

    added google search page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "lato": "'Lato', sans-serif",
+      },
+      backgroundColor: {
+        hover: "rgba(60,64,67,.08)"
+      },
+      boxShadow: {
+        "input-shadow": "0 1px 6px rgb(32 33 36 / 28%)"
+      }
+    },
   },
   plugins: [],
 }
