f608a0fe91497dd05819b2d6394c1df512985f6c
izzett222 <iizzeddin62@gmail.com>
2022-10-18 12:49:37 +0200

    add color and spacing into the config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        "lato": "'Lato', sans-serif",
+        "arial": "arial"
       },
       backgroundColor: {
         hover: "rgba(60,64,67,.08)"
@@ -14,7 +14,44 @@ module.exports = {
       },
       colors: {
         "menu-link": "rgba(0,0,0,.87)",
-        "active-menu-link": "#4285f4"
+        "active-menu-link": "#4285f4",
+        "light-gray": "#70757a",
+        "input-border": "#dfe1e5",
+        "btn-bg": "#f8f9fa",
+        "btn-text": "#3c4043",
+        "text-light-gray": "#4d5156",
+        "link-blue": "#1a0dab",
+        "footer-bg": "#f2f2f2",
+        "dark-gray": "#dadce0"
+      },
+      borderWidth: {
+        "1\.5": "3px"
+      },
+      fontSize: {
+        '0\.5sm': "13px",
+        "15": "15px",
+        "1\.5": "3px"
+      },
+      spacing: {
+        "0\.88": "0.188rem",
+        "2.7": "0.688rem"
+      },
+      padding: {
+        '4\.5': "15px",
+        "18": "1.125rem",
+        "7.5": "1.875rem"
+      },
+      margin: {
+        "2.7": "0.688rem"
+      },
+      height: {
+        "24\.5": "5.75rem"
+      },
+      width: {
+        "64\.5": "17rem"
+      },
+      maxWidth: {
+        "36": "36.375rem"
       }
     },
   },
